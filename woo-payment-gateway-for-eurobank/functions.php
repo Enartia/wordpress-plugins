61d3feaf90c673a6e1346990d71916f7abbe4ed0
Mathijs Futselaar <mathijs.futselaar@proserve.nl>
2024-03-06 14:54:46 +0100

    version numbers

--- woo-payment-gateway-for-eurobank/functions.php
+++ woo-payment-gateway-for-eurobank/functions.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Plugin Name: Eurobank WooCommerce Payment Gateway
+Plugin URI: https://www.papaki.com
+Description: Eurobank Payment Gateway allows you to accept payment through various channels such as Maestro, Mastercard, AMex cards, Diners and Visa cards On your Woocommerce Powered Site.
+Version: 2.0.0
+Author: Papaki
+Author URI: https://www.papaki.com
+License: GPL-3.0+
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+WC tested: 8.5.0
+Text Domain: woo-payment-gateway-for-eurobank
+Domain Path: /languages
+ */
 
 function eurobank_message()
 {
