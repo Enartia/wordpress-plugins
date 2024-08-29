c9dc84b5ae3c125fa7af86c1dcc8b503737ca4c3
Mathijs Futselaar <mathijs.futselaar@nl.team.blue>
2024-08-29 12:26:14 +0200

    Removed misinterpreted mandatory fields and updated translations

--- woo-payment-gateway-for-piraeus-bank/classes/WC_Piraeusbank_Gateway.php
+++ woo-payment-gateway-for-piraeus-bank/classes/WC_Piraeusbank_Gateway.php
@@ -880,23 +880,10 @@ class WC_Piraeusbank_Gateway extends WC_Payment_Gateway
      */
     public function validate_fields()
     {
-        $requiredFields = [
-            'billing_email' => 'E-mail address',
-            'billing_city' => 'Billing town/city',
-            'billing_country' => 'Billing country / region',
-            'billing_state' => 'Billing state / county',
-            'billing_address_1' => 'Billing street address',
-            'billing_postcode' => 'Billing postcode / ZIP'
-        ];
-
         if (is_checkout() && WC()->session->get('chosen_payment_method') === $this->id && $this->pb_cardholder_name === 'yes') {
-            $requiredFields['cardholder_name'] = 'Cardholder Name';
-        }
-
-        foreach ($requiredFields as $field => $info) {
-            if (!isset($_POST[$field]) || trim($_POST[$field]) === '') {
+            if (!isset($_POST['cardholder_name']) || trim($_POST['cardholder_name']) === '') {
                 wc_add_notice(
-                    __($info . ' is a mandatory field!'),
+                    __('Cardholder name is mandatory', self::PLUGIN_NAMESPACE),
                     'error'
                 );
             }
--- woo-payment-gateway-for-piraeus-bank/wooshop-piraeus.php
+++ woo-payment-gateway-for-piraeus-bank/wooshop-piraeus.php
@@ -3,7 +3,7 @@
   Plugin Name: Piraeus Bank WooCommerce Payment Gateway
   Plugin URI: https://www.papaki.com
   Description: Piraeus Bank Payment Gateway allows you to accept payment through various channels such as Maestro, Mastercard, AMex cards, Diners  and Visa cards On your Woocommerce Powered Site.
-  Version: 2.0.6
+  Version: 2.0.7
   Author: Papaki
   Author URI: https://www.papaki.com
   License: GPL-3.0+
